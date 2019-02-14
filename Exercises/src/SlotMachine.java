8817b231587e52eff340535e5f80115e0f309b8a
rodrigojimc <rodrigo.jimenez.correa.@gmail.com>
2019-02-14 12:02:35 -0600

    OOP
    Restructuring

--- Exercises/src/SlotMachine.java
+++ Exercises/src/SlotMachine.java
@@ -1,26 +1,36 @@
 // @author RodrigoJimenezCorrea
 
-import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Random;
 
 public class SlotMachine {
 	
+	static int credits = 0;
+	static int bet = 1;
 	
-	public static int prize (int n1, int n2, int n3) {
+	static Scanner scan = new Scanner(System.in);
+	static String input;
+	static boolean validOption;
+	
+	static Random random = new Random();
+	static int combination[] = {4,4,4};
+	
+	static int payout;
+	
+	
+	public static int prize (int combination[]) {
 		
 		int payout = 0;
 		int fours = 0;
-		int n[] = {n1,n2,n3};
 		
-		if (n1==n2 && n2==n3) {
-			if (n1==4) { payout = 30; }
+		if (combination[0]==combination[1] && combination[1]==combination[2]) {
+			if (combination[0]==4) { payout = 30; }
 			else { payout = 10; }	
 		} 
 		
 		else {
 			for(int i=0; i<3; i++) {
-				if (n[i] == 4) { fours++; }
+				if (combination[i] == 4) { fours++; }
 			}
 			if (fours == 1) { payout = 1; }
 			else if (fours == 2) { payout = 4; }
@@ -29,168 +39,168 @@ public class SlotMachine {
 		return payout;
 	}
 	
-	public static void main(String[] args) {
+	
+	public static void printMenu() {
 		
-		int credits = 0;
-		int bet = 1;
-		int payout;
+		System.out.println("\n| [4th SLOT!] |");
+		System.out.println("---------------");
+		System.out.println(">>>["+combination[0]+"]["+combination[1]+"]["+combination[2]+"]<<<");
+		System.out.println("---------------");
+		System.out.println("Credits: "+credits);
+		System.out.println("Bet: "+bet);
+		System.out.println("---------------");
+		System.out.println("[A]dd Credit");
+		System.out.println("[P]lay ");
+		System.out.println("[S]et Bet ");
+		System.out.println("[C]ash ");
+		System.out.println("[R]ules ");
+		System.out.println("[T]urn off ");
+		System.out.println("---------------");
+		System.out.print("Option: ");
 		
-		Scanner scan = new Scanner(System.in);
-		String input;
+	}
+	
+	public static void printRules() {
 		
-		String[] options = {"A","P","S","C","R","T"};
-		boolean validOption;
+		System.out.println("\nCombination\tPays");
+		System.out.println("----------------------");
+		System.out.println(" [4][4][4]\tx30");
+		System.out.println(" [n][n][n]\tx10");
+		System.out.println(" [4][4][x]\tx4");
+		System.out.println(" [4][x][x]\tx1");
 		
-		Random random = new Random();
-		
-		int n1 = 4;
-		int n2 = 4;
-		int n3 = 4;
+	}
 
+	
+	public static void addCredits(){
+		
 		do {
-		
-			System.out.println("\n| [4th SLOT!] |");
-			System.out.println("---------------");
-			System.out.println(">>>["+n1+"]["+n2+"]["+n3+"]<<<");
-			System.out.println("---------------");
-			System.out.println("Credits: "+credits);
-			System.out.println("Bet: "+bet);
-			System.out.println("---------------");
-			System.out.println("[A]dd Credit");
-			System.out.println("[P]lay ");
-			System.out.println("[S]et Bet ");
-			System.out.println("[C]ash ");
-			System.out.println("[R]ules ");
-			System.out.println("[T]urn off ");
-			System.out.println("---------------");
-			System.out.print("Option: ");
-		
-			input = scan.next();
-			validOption = Arrays.stream(options).anyMatch(input:: equals);
 			
-			if (!validOption) {
+			System.out.print("\nCoin: $");
+			input = scan.next();
+			
+			if (!input.matches("[125]")) {
 				
-				System.out.println("\nInvalid Option!");
-				System.out.println("Please try again");
+				validOption = false;
+				System.out.println("\nOnly accepts \n$1,$2 and $5");
 				
 			}
 			
+			else {
+				
+				validOption = true;
+				credits += Character.getNumericValue(input.charAt(0));
+				
+				if (credits>99) { credits=99; }
+				
+			}
+			
+		} while(!validOption);
+		
+	}
+	
+	public static void setBet() {
+		
+		do {
+			
+			System.out.print("\nNew bet: ");
+			input = scan.next();
+			
+			if (!input.matches("[12345]")) {
+				
+				validOption = false;
+				
+				System.out.println("\nBet must be a");
+				System.out.println("number from 1 to 5");
+				
+			}
+			
+			else {
+				
+				validOption = true;
+				bet = Character.getNumericValue(input.charAt(0));
+				
+			}
+			
+		} while(!validOption);
+		
+		
+	}
+	
+	public static void cash() {
+		
+		if (credits>0) {
+			
+			System.out.println("\nCredits payed: "+credits);
+			System.out.println("Come back later!");
+			
+		}
+	}
+	
+	
+	public static void play() {
+		
+		if (credits >= bet) {
+			
+			credits -= bet;
+			
+			for (int i=0; i<3; i++) {
+				combination[i] =random.nextInt(6);
+			}
+			
+			payout = bet*prize(combination);
+			credits += payout;
+
+			switch (payout){
+			
+			case 0:
+				System.out.println("\nBetter luck next time!");
+				break;
+			case 1:
+				System.out.println("\nYou won 1 credit!");
+				break;
+			default:
+				System.out.println("\nYou won "+payout+" credits!");
+			}
+		}
+		else {
+			System.out.println("\nNo enought credits!");
+		}
+		
+	}
+	
+
+	public static void main(String[] args) {
+
+		do {
+		
+			printMenu();
+		
+			input = scan.next();
+			
 			switch (input) {
 			
-			case "A":
-				
-				do {
-					
-					System.out.print("\nCoin: $");
-					input = scan.next();
-					
-					if (!input.matches("[125]")) {
-						
-						validOption = false;
-						System.out.println("\nOnly accepts \n$1,$2 and $5");
-						
-					}
-					
-					else {
-						
-						validOption = true;
-						credits += Character.getNumericValue(input.charAt(0));
-						
-						if (credits>99) { credits=99; }
-						
-					}
-					
-				} while(!validOption);
-				
-				break;
+			case "A": addCredits(); break;
 			
-			case "P":
+			case "P": play(); break;
 				
-				if (credits >= bet) {
-					
-					credits -= bet;
-					
-					n1=random.nextInt(6);
-					n2=random.nextInt(6);
-					n3=random.nextInt(6);
-					
-					payout = bet*prize(n1,n2,n3);
-					credits += payout;
-
-					switch (payout){
-					
-					case 0:
-						System.out.println("\nBetter luck next time!");
-						break;
-					case 1:
-						System.out.println("\nYou won 1 credit!");
-						break;
-					default:
-						System.out.println("\nYou won "+payout+" credits!");
-					}
-				}
-				else {
-					System.out.println("\nNo enought credits!");
-				}
+			case "S": setBet(); break;
+				
+			case "C": cash(); break;
+				
+			case "R": printRules(); break;
+				
+			case "T":
+				
+				System.out.println("\n< Turned off >");
 				
 				break;
 				
-			case "S":
+			default:
 				
-				do {
-					
-					System.out.print("\nNew bet: ");
-					input = scan.next();
-					
-					if (!input.matches("[12345]")) {
-						
-						validOption = false;
-						
-						System.out.println("\nBet must be a");
-						System.out.println("number from 1 to 5");
-						
-					}
-					
-					else {
-						
-						validOption = true;
-						bet = Character.getNumericValue(input.charAt(0));
-						
-					}
-					
-				} while(!validOption);
-				
-				break;
-				
-			case "C":
-				
-				if (credits>0) {
-					
-					System.out.println("\nCredits payed: "+credits);
-					System.out.println("Come back later!");
-					
-				}
-				
-				else {
-					
-					System.out.println("\nNo credits!");
-					
-				}
-				
-				credits=0;
-				
-				break;
-				
-			case "R":
-				
-				System.out.println("\nCombination\tPays");
-				System.out.println("----------------------");
-				System.out.println(" [4][4][4]\tx30");
-				System.out.println(" [n][n][n]\tx10");
-				System.out.println(" [4][4][x]\tx4");
-				System.out.println(" [4][x][x]\tx1");
-				
-				break;
+				System.out.println("\nInvalid Option!");
+				System.out.println("Please try again");
+			
 			}
 			
 		} while(input.compareTo("T")!=0);
